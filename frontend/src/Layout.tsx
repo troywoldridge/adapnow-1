e5a95d57311ab52632fa923687f12eeeb5e58d48
troywoldridge <troy-woldridge-1@gmail.com>
2025-02-15 12:40:20 -0500

    second push

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "/Navbar";
 import Footer from "./Footer";
 import NavbarCategories from "./NavbarCategories";
 import SidebarMenu from "./SidebarMenu";
