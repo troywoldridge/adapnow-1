0f3c92b51bf52456793f0f933eb5b267bdb303ea
troywoldridge <troy-woldridge-1@gmail.com>
2025-02-16 19:44:06 -0500

    evening commit

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import algoliasearch from "algoliasearch/lite";
-import {
-  InstantSearch,
-  SearchBox,
-  Configure
-} from "react-instantsearch-dom";
+import { InstantSearch, SearchBox, Configure } from "react-instantsearch-dom";
 import Image from 'next/image';
 
 // Import the CustomHits component
@@ -13,6 +9,21 @@ import { CustomHits } from "@/components/CustomHits/CustomHits";
 // Algolia credentials
 const searchClient = algoliasearch("12N0JD5MJD", "198c6e8c46b1332bc153a07585c84872");
 
+// Initialize the 'products' index
+const index = searchClient.initIndex('products');
+
+// Perform a search to see if the index exists and contains data
+index
+  .search('')
+  .then(({ hits }) => {
+    // If data exists in the index, it will be logged here
+    console.log('Index exists and data:', hits);
+  })
+  .catch((err) => {
+    // If there is an error, such as the index not existing, it will be logged here
+    console.error('Index does not exist or error:', err);
+  });
+
 export default function SearchPage() {
   return (
     <div style={{ padding: 20 }}>
@@ -20,8 +31,7 @@ export default function SearchPage() {
       
       <InstantSearch
         searchClient={searchClient}
-        indexName="YourIndexName"
-         // insights={true} // Enable event tracking
+        indexName="products"
       >
         {/* Configure the number of results per page */}
         <Configure hitsPerPage={8} />
